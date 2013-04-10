df264ad8a150ff9c60b623461c34a14eb47dc8ee
Sergey Tyuterev <altruist007@mail.ru>
2013-04-11 00:30:51 +0400

    testing commiting

---
+++ Files.py
@@ -0,0 +1,35 @@
+__author__ = 'Serge'
+
+from Smatrix import *
+
+
+def readSmatFile(name, size):
+    res = Smatrix(size)
+    res.fillSmat()
+    rFile = open(name, 'r')
+    arr = rFile.read().split()
+    for i in xrange(size):
+        for j in xrange(size):
+            res.getCont()[i][j] = int(arr[j*size + i])
+    return res
+
+
+def FileCheck(name, number, type):
+    try:
+        open(name + str(number) + '.' + type, 'r')
+        print 'file exists'
+        return 0
+    except IOError:
+        print 'file ok'
+        return -1
+    pass
+
+
+def FindNumber(name, type='txt'):
+    fileExists = 0
+    number = 1
+    while fileExists != -1:
+        number += 1
+        fileExists = FileCheck(name, number, type)
+    print 'number found'
+    return number
